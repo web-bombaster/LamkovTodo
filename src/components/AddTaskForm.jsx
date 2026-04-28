33b3d6f4fdcab6ffccb296afa13dece2132298b3
Yurij Moshatin <y.moshatin@rbru-test.ru>
2026-04-28 16:11:53 +0300

    Props в React, children

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -4,8 +4,12 @@ import Field from "./Field"
 const AddTaskForm = () => {
 	return (
 		<form className="todo__form">
-			<Field />
-			<Button />
+			<Field
+				className="todo__field"
+				label="New task title"
+				id="new-task"
+			/>
+			<Button type='submit'>Add</Button>
 		</form>
 	)
 }
