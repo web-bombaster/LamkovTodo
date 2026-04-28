b59094ad4f3209cd62467e693f4177ef5a1dd2ae
Yurij Moshatin <y.moshatin@rbru-test.ru>
2026-04-28 17:55:02 +0300

    События в React: onClick, onChange

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,8 +6,21 @@ import TodoList from "./TodoList"
 const Todo = () => {
 	const tasks = [
 		{ id: 'task-1', title: 'Купить молока', isDone: false },
-		{ id: 'task-w', title: 'Погладить кота', isDone: true },
+		{ id: 'task-2', title: 'Погладить кота', isDone: true },
 	]
+
+	const deleteTasks = () => {
+		console.log('Удаляем все задачи')
+	}
+
+	const deleteTask = (taskId) => {
+		console.log(`Удаляем задачу с id: ${taskId}`)
+	}
+
+	const toggleTaskComplete = (taskId, isDone) => {
+		console.log(`Задача ${taskId} ${isDone ? 'выполнена' : 'не выполнена'}`)
+	}
+
 	return (
 		<div className="todo">
 			<h1 className="todo__title">To Do List</h1>
@@ -16,8 +29,13 @@ const Todo = () => {
 			<TodoInfo
 				total={tasks.length}
 				done={tasks.filter(({ isDone }) => isDone).length}
+				onDeleteAllButtonClick={deleteTasks}
 			/>
-			<TodoList tasks={tasks} />
+			<TodoList
+				tasks={tasks}
+				onDeleteTaskButtonClick={deleteTask}
+				onTaskCompleteChange={toggleTaskComplete}
+			/>
 		</div>
 	)
 }
--- src/components/TodoInfo.jsx
+++ src/components/TodoInfo.jsx
@@ -2,6 +2,7 @@ const TodoInfo = (props) => {
 	const {
 		total,
 		done,
+		onDeleteAllButtonClick,
 	} = props
 
 	const hasTasks = total > 0
@@ -11,7 +12,7 @@ const TodoInfo = (props) => {
 			<div className="todo__total-tasks">
 				Done {done} from {total}
 			</div>
-			{hasTasks && <button className="todo__delete-all-button" type="button">Delete all</button>}
+			{hasTasks && <button className="todo__delete-all-button" type="button" onClick={onDeleteAllButtonClick}>Delete all</button>}
 		</div>
 	)
 }
